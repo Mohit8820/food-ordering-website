0107c2624c5462b6b1554c0cb9fd9a2b974f2f5c
Mohit8820 <mohitprasadsah8820@gmail.com>
2023-07-06 03:02:50 +0530

    auth

--- js/auth.js
+++ js/auth.js
@@ -1,20 +1,171 @@
 "use strict";
+const cityList = {
+  bh: ["Gaya", "Muzaffarpur", "Nalanda", "Patna", "Rohtas"],
+  jh: ["Bokaro", "Dhanbad", "Hazaribagh", "Ramgarh", "Ranchi"],
+  kt: ["Bagalkot", "Bangalore", "Belgaum", "Bellary", "Bidar"],
+  tn: ["Ariyalur", "Chennai", "Coimbatore", "Cuddalore", "Dharmapuri"],
+  mh: ["Amravati", "Jalna", "Kolhapur", "Latur", "Mumbai", "Nagpur"],
+  wb: ["Darjeeling", "Hooghly", "Howrah", "Jalpaiguri", "Kolkata"],
+  dl: [
+    "Central Delhi",
+    "East Delhi",
+    "North Delhi",
+    "South Delhi",
+    "West Delhi",
+  ],
+};
+var users;
 
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
-const forms = document.querySelectorAll(".needs-validation");
+/*************populating cities********* */
+function autoPopulateCities() {
+  document.getElementById("select-state").addEventListener("change", () => {
+    console.log("abc");
+    var StateSelected = document.getElementById("select-state").value;
+    var htmlString = `<option value="" selected="" disabled="" hidden="">
+  Choose City
+</option>`;
+    for (var i = 0; i < cityList[StateSelected].length; i++) {
+      htmlString =
+        htmlString +
+        "<option value='" +
+        cityList[StateSelected][i] +
+        "'>" +
+        cityList[StateSelected][i] +
+        "</option>";
+    }
+    document.getElementById("select-city").innerHTML = htmlString;
+  });
+}
+/*************populating cities********* */
 
-// Loop over them and prevent submission
-Array.from(forms).forEach((form) => {
-  form.addEventListener(
-    "submit",
-    (event) => {
-      if (!form.checkValidity()) {
+function getUsers() {
+  if (localStorage) {
+    if (!localStorage.tastyExUsers) {
+      localStorage.setItem("tastyExUsers", JSON.stringify([]));
+    }
+    users = JSON.parse(localStorage.getItem("tastyExUsers"));
+  } else {
+    alert("Unsupported browser.");
+  }
+  console.log(users);
+}
+
+function currentUserIndex() {
+  if (users.length > 0) {
+    var ind = users.findIndex((user) => user.isLoggedIn == true);
+    return ind;
+  } else return -1;
+}
+
+//var signupForm = document.signupForm;
+function blurValidation() {
+  const onBlurInputs = document.querySelectorAll(".validate-onblur");
+  Array.from(onBlurInputs).forEach((input) => {
+    input.addEventListener("focusout", (e) => {
+      console.log(input.checkValidity());
+      input.classList.remove("is-valid");
+      input.classList.remove("is-invalid");
+      input.classList.add(input.checkValidity() ? "is-valid" : "is-invalid");
+    });
+  });
+}
+
+function submitValidation() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
         event.preventDefault();
-        event.stopPropagation();
-      }
 
-      form.classList.add("was-validated");
-    },
-    false
-  );
+        if (!form.checkValidity()) {
+          event.stopPropagation();
+        } else {
+          if (form.name == "signupForm") signupFunc();
+          if (form.name == "loginForm") loginFunc();
+        }
+
+        form.classList.add("was-validated");
+        console.log(form.name);
+      },
+      false
+    );
+  });
+}
+
+function signupFunc() {
+  var user = {
+    isLoggedIn: true,
+    fname: document.getElementById("firstName").value,
+    lname: document.getElementById("lastName").value,
+    pwd: document.getElementById("signupPassword").value,
+    email: document.getElementById("signupEmail").value,
+    phnNo: document.getElementById("phoneNumber").value,
+    city: document.getElementById("select-city").value,
+    state: document.getElementById("select-state").value,
+    pin: document.getElementById("pinCode").value,
+    add1: document.getElementById("address1").value,
+    add2: document.getElementById("address2").value,
+    cart: { dishes: [], quantity: [] },
+    prevOrders: [],
+  };
+  const checkEmail = users.some((u) => user.email === u.email);
+  if (checkEmail) {
+    alert("User already exists. Please Log in");
+    return;
+  }
+  users.forEach((user) => {
+    user.isLoggedIn = false;
+  });
+  users.push(user);
+  localStorage.setItem("tastyExUsers", JSON.stringify(users));
+  window.location.reload();
+}
+function loginFunc() {
+  const email = document.getElementById("loginEmail").value;
+  const pass = document.getElementById("loginPassword").value;
+  var userIndex = users.findIndex((u) => u.email === email);
+  console.log(users);
+  console.log(userIndex);
+  if (userIndex >= 0) {
+    if (users[userIndex].pwd === pass) {
+      users.forEach((user) => {
+        user.isLoggedIn = false;
+      });
+      users[userIndex].isLoggedIn = true;
+      localStorage.setItem("tastyExUsers", JSON.stringify(users));
+      //toggle logout btn
+      window.location.reload();
+    } else alert("Invalid Password");
+  } else alert("User doesn't exist");
+}
+function logoutFunc() {
+  console.log("logged out");
+  users[currentUserIndex()].isLoggedIn = false;
+  localStorage.setItem("tastyExUsers", JSON.stringify(users));
+  toggleLogoutbtn("logout");
+}
+function toggleLogoutbtn(action) {
+  if (action === "login") {
+    document.getElementById("login-btn").classList.add("d-none");
+    document.getElementById("signup-btn").classList.add("d-none");
+    document.getElementById("logout-btn").classList.remove("d-none");
+  } else {
+    document.getElementById("login-btn").classList.remove("d-none");
+    document.getElementById("signup-btn").classList.remove("d-none");
+    document.getElementById("logout-btn").classList.add("d-none");
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  blurValidation();
+  submitValidation();
+  autoPopulateCities();
+  getUsers();
+  if (currentUserIndex() >= 0) {
+    toggleLogoutbtn("login");
+  }
 });
+export { currentUserIndex };
---
+++ js/order.js
@@ -0,0 +1,130 @@
+var users;
+var curUser;
+
+//one modal whose body changes as per button click
+//same for toast
+var menu;
+var filterMenu;
+
+let restaurantNames = [
+  "all",
+  "kfc",
+  "haldiram",
+  "kaveri",
+  "dominos",
+  "pizza hut",
+  "barbeque nation",
+  "mcdonald's",
+];
+let restaurantImages = [
+  "all.gif",
+  "kfc.avif",
+  "haldiram.png",
+  "kaveri.avif",
+  "dominos.avif",
+  "pizzahut.avif",
+  "bbq.png",
+  "McDonalds.png",
+];
+let restContainer = document.getElementById("rest-list");
+restaurantNames.forEach((rest, i) => {
+  restContainer.innerHTML += `<div class="restaurant">
+          <div class="rest-img ${i == 0 && "active-rest"}">
+            <img src="./img/restaurants/${restaurantImages[i]}" alt="${
+    restaurantImages[i]
+  }" />
+          </div>
+          <h4 class="center-text">${rest}</h4>
+        </div>`;
+});
+
+let restaurants = document.querySelectorAll(".restaurant");
+restaurants.forEach((restaurant, index) => {
+  restaurant.addEventListener("click", function handleRestaurantClick(event) {
+    for (const rest of document.querySelectorAll("div.active-rest")) {
+      rest.classList.remove("active-rest");
+    }
+    restaurant
+      .getElementsByClassName("rest-img")[0]
+      .classList.add("active-rest");
+    filterDishes(index);
+  });
+});
+
+function filterDishes(i) {
+  if (restaurantNames[i] == "all") {
+    filterMenu = menu;
+    // appendDishes(menu);
+  } else {
+    filterMenu = menu.filter(function (dish) {
+      return dish.restaurant.toLowerCase() == restaurantNames[i].toLowerCase();
+    });
+    // appendDishes(filterMenu);
+  }
+  searchDish();
+}
+
+let dishContainer = document.getElementById("dish-list");
+function appendDishes(dishes) {
+  dishContainer.innerHTML = "";
+  dishes.forEach((dish, i) => {
+    var id = dish.dishId;
+    dishContainer.innerHTML += `
+      <div class="dish">
+        <div class="dish-img">
+          <img
+            src="${"img/gallery/gallery-" + ((i + 1) % 12)}.jpg"
+            alt="dish-image"
+          />
+          <div class="rating">
+            ${dish.rating}
+            <ion-icon name="star" aria-hidden="true"></ion-icon>
+          </div>
+        </div>
+        <h4>${dish.name}</h4>
+        <p>${dish.description}</p>
+        <div class="dish-footer">
+          <div class="price">₹${dish.price}</div>
+          <button data-id="${
+            dish.dishId
+          }" onclick="addDish(event);">Add</button>
+        </div>
+      </div>`;
+  });
+}
+function searchDish() {
+  let searchedText = document.getElementById("dish-search-box").value;
+  if (searchedText === "") appendDishes(filterMenu);
+  else {
+    searchedMenu = filterMenu.filter(function (dish) {
+      return dish.name.toLowerCase().indexOf(searchedText.toLowerCase()) !== -1;
+    });
+    appendDishes(searchedMenu);
+  }
+}
+
+function addDish(e) {
+  // check for log in
+  var dishId = e.target.getAttribute("data-id");
+  var dish = menu.find((dish) => dish.dishId == dishId);
+  console.log(dish);
+}
+function loadCart() {
+  // check for log in
+  console.log("load");
+}
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("menu.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      menu = data;
+      filterMenu = data;
+      appendDishes(data);
+    })
+    .catch(function (err) {
+      console.log("error: " + err);
+    });
+});
